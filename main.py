ff34eb9c41dccc90756ec24e486660373e42dec7
Devah Schaefers <73009338+devahschaefers@users.noreply.github.com>
2022-04-01 11:28:28 -0700

    fixed printShapeFunction

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 import os
 import pathlib
 import copy
+import time
 
 
 PATH = pathlib.Path().absolute()
@@ -31,6 +32,7 @@ def printBoard(board):
 class Block:
   def __init__(self, color, location = "s"):
     self.color = color
+    self.value = color.value
     self.location = location
     
 class Shape(Block):
@@ -40,7 +42,7 @@ class Shape(Block):
       self.id = 0 # will be assigned when it is spawned
       self.name = name
       self.currentRotation = 0
-      
+
       if (color.value != 0):
         self.isEmpty = False
       else:
@@ -137,7 +139,6 @@ rawShapes = {
                   [Block(Colors.BLUE), Block(Colors.BLUE), Block(Colors.NO_COLOR)],  
                   [Block(Colors.NO_COLOR), Block(Colors.BLUE), Block(Colors.NO_COLOR)],  
                   [Block(Colors.NO_COLOR), Block(Colors.BLUE), Block(Colors.NO_COLOR)]   
-                  
                 ]
               ],
     "sBlock": [
@@ -238,23 +239,30 @@ class Game():
     #---------------------------------
     
     def drawDrawShapeAtLocation(self, location, Tetromino = None):
-      ##assumes tetormino is a shape object
+      #create copy of the board which will replace the actual board if the fuction doesnt encounter errors
+      board = copy.deepcopy(self.board)
+#assumes tetormino is a shape object
       if Tetromino == None:
         Tetromino = self.currentTetrominoFalling[0]
       y_lengthOfTetromino = len(Tetromino.shape[Tetromino.currentRotation])
       x_lengthOfTetromino = len(Tetromino.shape[Tetromino.currentRotation][0])
       j = 0 #x
       i = 0 #y
+     
       for y in range(location[1], location[1] - y_lengthOfTetromino, -1):
         for x in range(location[0], location[0]+ x_lengthOfTetromino):
-          self.board[x][y] = Tetromino.shape[Tetromino.currentRotation][j][i]
+          ignoreCurrentBlock = False
+          if self.board[x][y].color != Colors.NO_COLOR:
+            return -1
+          if Tetromino.shape[Tetromino.currentRotation][j][i].value == 0:
+            ignoreCurrentBlock = True
+          if ignoreCurrentBlock == False:
+            board[x][y] = Tetromino.shape[Tetromino.currentRotation][j][i]
           i += 1
         j += 1
         i = 0
+      self.board = board
     
-    def setupgame(self):
-        pass
-
     def draw(self):
         pass
     
@@ -262,9 +270,18 @@ class Game():
       self.numberOfTetrominosSpawned += 1
       Tetromino.id = self.numberOfTetrominosSpawned
       self.currentTetrominoFalling = [Tetromino, Tetromino.id]
+      self.drawDrawShapeAtLocation([3, 39], Tetromino)
+      return Tetromino.name
       
 
 game = Game()
-while True:
-    printBoard(game.board)
-    os.system(f"{}".PATH)
+
+
+  
+if game.currentTetrominoFalling == None:
+    spawmedTetromino = game.spawnTetromino()
+    print("Spawned Tetromino:", spawmedTetromino)
+print(game.currentTetrominoFalling[0].name)
+printBoard(game.board)
+time.sleep(0.5)
+# os.system("clear")
